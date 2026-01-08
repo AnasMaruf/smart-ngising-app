fb23edb4f4eb8bb0b64c4f0f9b60d3f956b9035e
Abdoor <abduromanov11@gmail.com>
2026-01-09 00:34:02 +0700

    feat: init gluestack ui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
+import { GluestackUIProvider } from "@/lib/gluestack/providers";
 import { Stack } from "expo-router";
 
+import '../global.css';
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
---
+++ src/lib/gluestack/providers/index.ts
@@ -0,0 +1,1 @@
+export { default as GluestackUIProvider } from './GluestackUIProvider';
